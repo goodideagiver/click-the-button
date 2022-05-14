cae43ac444226473433670880124567827739d42
prplblck <kariurek@gmail.com>
2022-05-14 20:46:10 +0200

    improved success message

--- src/scripts/formSuccesHandler.js
+++ src/scripts/formSuccesHandler.js
@@ -2,10 +2,21 @@ import { resetLocalStorage } from './localStorageHandler';
 import { hideModal } from './modal';
 import { setUserValid } from './index';
 import showAlert from './showAlert';
+import { getTriesCount } from './getTriesCount';
+import { handleTriesNumeralEndChar } from './handleTriesNumeralEndChar';
 
 export const formSuccesHandler = () => {
-	showAlert('You have successfully logged in!');
+	showSuccessAlert();
 	setUserValid();
 	resetLocalStorage();
 	hideModal();
+};
+
+const showSuccessAlert = () => {
+	const tries = getTriesCount();
+	showAlert(
+		`You have succesfully logged in! You did it in ${tries} ${handleTriesNumeralEndChar(
+			tries
+		)}!`
+	);
 };
